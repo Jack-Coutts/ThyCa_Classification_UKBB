1a05cb6cca1c7ff63fc28d0366190cb3833f7912
jackcoutts <jackwgcoutts@gmail.com>
2022-07-17 22:55:34 +0100

    Added one hot encoding function.

--- ML_functions.py
+++ ML_functions.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+import sklearn.neighbors._base  # Import required for missforest due to new sklearn version renaming
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+from missingpy import MissForest  # Import package for MissForest
+
+
 
 # Manual feature selection from tsv
 
@@ -30,8 +35,35 @@ def feature_encoding(one_hot_cols, df):
     return dataframe
 
 
+# Function to get column names for continuous and non-continuous (categorical) features
+def find_cat_or_con_columns(dataframe):
+
+    columns = [list(dataframe[i]) for i in dataframe] # Nested list of column values
+
+    uniques = [len(set([i for i in a if pd.notna(i)])) for a in columns] # Num of unique values in a column
+
+    continuous_indexes = [i for i, c in enumerate(uniques) if c > 100] # Indexes of continuous columns
+
+    categorical_indexes = [i for i, c in enumerate(uniques) if c <= 100] # Indexes of categorical columns
+
+    con_cols = [dataframe.columns[x] for x in continuous_indexes] # List containing continuous columns names
+
+    cat_cols = [dataframe.columns[x] for x in categorical_indexes] # List containing categorical columns names
+
+    return con_cols, cat_cols # return two lists of continuous & categorical column names
+
 
 # Imputation
+def miss_forest_imputation(df):
+
+    con, cat = find_cat_or_con_columns(df)
+
+    imputer = MissForest(max_iter=5)  # Initiate imputer
+    imputed = imputer.fit_transform(df, cat_vars=cat)  # Carry out imputation, specified categorical columns
+
+
+
+    pass
 
 
 # Feature scaling
