db992c7e4df894ef8800dccc3833f79e1a84b727
jackcoutts <jackwgcoutts@gmail.com>
2022-07-18 13:27:54 +0100

    Testing out ML functions.

--- ML_functions.py
+++ ML_functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import sklearn.neighbors._base  # Import required for missforest due to new sklearn version renaming
+import sys
 sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
 from missingpy import MissForest  # Import package for MissForest
 
@@ -19,20 +20,6 @@ def split_tsv(file_path, sep='\t', test_size=0.2):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=True)
 
     return X_train, X_test, y_train, y_test
-
-
-# Feature encoding - one hot encoding (label encoding already completed)
-def feature_encoding(one_hot_cols, df):
-
-    enc = OneHotEncoder()  # Initiate onehotencoder instance
-    enc_data = enc.fit_transform(df[one_hot_cols]).toarray()  # One hot encode target cols
-    enc_feat = enc.get_feature_names(one_hot_cols)  # Add new column names
-    encoded_df = pd.DataFrame(enc_data, columns=enc_feat)  # Convert results to dataframe
-    encoded_df = encoded_df.set_index(df.index)  # Ensure new df has same index as original
-    df_con = df.drop(one_hot_cols, axis=1)  # remove original columns that were encoded
-    dataframe = df_con.join(encoded_df, how='inner')  # Combine the two dataframes
-
-    return dataframe
 
 
 # Function to get column names for continuous and non-continuous (categorical) features
@@ -64,6 +51,23 @@ def miss_forest_imputation(df):
     imputed_df = pd.DataFrame(imputed, columns=df.columns)
 
     return imputed_df
+
+
+# Feature encoding - one hot encoding (label encoding already completed)
+def feature_encoding(one_hot_cols, df):
+
+    enc = OneHotEncoder()  # Initiate onehotencoder instance
+    enc_data = enc.fit_transform(df[one_hot_cols]).toarray()  # One hot encode target cols
+    enc_feat = enc.get_feature_names(one_hot_cols)  # Add new column names
+    encoded_df = pd.DataFrame(enc_data, columns=enc_feat)  # Convert results to dataframe
+    encoded_df = encoded_df.set_index(df.index)  # Ensure new df has same index as original
+    df_con = df.drop(one_hot_cols, axis=1)  # remove original columns that were encoded
+    dataframe = df_con.join(encoded_df, how='inner')  # Combine the two dataframes
+
+    return dataframe
+
+
+
 
 
 # Feature scaling
